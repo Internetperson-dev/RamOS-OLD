09435ce5641a9304fb9f73f77bbb1088c07961a6
Rameez10 <Rameezmajid10@outlook.com>
2022-03-24 16:36:32 +0000

    Update script.sh
    Fixed error where it adds Gnome packages that should stay removed

--- script.sh
+++ script.sh
@@ -2,10 +2,8 @@ sudo apt update
 sudo apt upgrade
 sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -yy
 sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
-sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
 gnome-software-plugin-flatpak -yy
 gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
-#set icons to minimize on click
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
