a7a573b7220de69dc051e9983efe236c968a25a1
Rameez10 <Rameezmajid10@outlook.com>
2022-03-24 16:39:53 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
-sudo apt update
-sudo apt upgrade
+#No more Gnome
 sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -yy
 sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
 gnome-software-plugin-flatpak -yy
@@ -7,6 +6,7 @@ gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+#Give me that RamOS UI
 sudo apt install tasksel
 sudo tasksel install kubuntu-desktop
 sudo apt-get remove nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot gnome* && sudo apt-get autoremove
