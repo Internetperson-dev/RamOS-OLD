dda480ca72be7cc4c64985825fde4d1bf5125323
Rameez10 <Rameezmajid10@outlook.com>
2022-03-28 16:53:09 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,8 +6,8 @@ gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+sudo apt-get remove nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot gnome* && sudo apt-get autoremove
 #Give me that RamOS UI
 sudo apt install tasksel
 sudo tasksel install kubuntu-desktop
-sudo apt-get remove nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot gnome* && sudo apt-get autoremove
 sudo apt update && sudo apt upgrade -y 
