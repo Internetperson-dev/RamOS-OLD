a28cec061435c06cc8317cd24a8378ae1ffd12a6
Rameez10 <Rameezmajid10@outlook.com>
2022-03-31 16:36:14 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #No more Gnome
-sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -yy
-sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
-gnome-software-plugin-flatpak -yy
+sudo apt purge gstreamer1.0-fluendo-mp3 deja-dup shotwell whoopsie whoopsie-preferences -y
+sudo snap remove gnome-characters gnome-calculator gnome-system-monitor -y
+gnome-software-plugin-flatpak -y
 gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt-get remove thunderbird rhythmbox ubuntu-web-launchers -y
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
-sudo apt-get remove nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot gnome* && sudo apt-get autoremove
+sudo apt-get remove nautilus nautilus-* gnome-power-manager gnome-screensaver gnome-termina* gnome-pane* gnome-applet* gnome-bluetooth gnome-desktop* gnome-sessio* gnome-user* gnome-shell-common zeitgeist-core libzeitgeist* gnome-control-center gnome-screenshot gnome* && sudo apt-get autoremove -y
 #Give me that RamOS UI
-sudo apt install tasksel
-sudo tasksel install kubuntu-desktop
+sudo apt install tasksel -y
+sudo tasksel install kubuntu-desktop -y
 sudo apt update && sudo apt upgrade -y 
 echo"Sorry but I am too lazy to automate this one but go to https://youtu.be/A0LiFu1eaMs?t=100 once you are in a webbrowser and just get the layan theme (also get the legacy kickoff start menu and the icons)
